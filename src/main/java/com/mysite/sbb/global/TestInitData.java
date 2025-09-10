ca1526d512bb000f9b6c27a9d76d2276fa299dd9
gandaraipower <jjangjongsoo@naver.com>
2025-09-10 17:41:45 +0900

    2-6

--- src/main/java/com/mysite/sbb/global/TestInitData.java
+++ src/main/java/com/mysite/sbb/global/TestInitData.java
@@ -1,5 +1,7 @@
-package com.mysite.sbb;
+package com.mysite.sbb.global;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,24 +1,31 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
+
+import com.mysite.sbb.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Answer {
+public class Question {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 200)
+    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @ManyToOne
-    private Question question;  //Question 엔티티를 참조하기 위해서
-}
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+    }
