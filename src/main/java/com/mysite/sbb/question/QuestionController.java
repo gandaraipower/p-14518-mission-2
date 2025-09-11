1de72b3237267adf1867543d5346959bef9fea9e
gandaraipower <jjangjongsoo@naver.com>
2025-09-11 17:03:50 +0900

    2-7

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/question/list")
+    public String list(Model model){
+        List<Question> questionList=questionService.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
+    }
+}
