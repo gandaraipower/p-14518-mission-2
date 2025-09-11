83880a34f3776146971180ff67f49eef4e76e67b
gandaraipower <jjangjongsoo@naver.com>
2025-09-11 17:14:37 +0900

    2-10

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,9 +15,16 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/question/list")
-    public String list(Model model){
-        List<Question> questionList=questionService.findAll();
-        model.addAttribute("questionList",questionList);
+    public String list(Model model) {
+        List<Question> questionList = questionService.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,8 +2,9 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import com.mysite.sbb.global.DataNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +14,14 @@ public class QuestionService {
 
     public List<Question> findAll() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
